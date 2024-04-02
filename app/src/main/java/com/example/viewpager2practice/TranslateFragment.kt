99ebbeda25f4a035910b074a2710a3da1a33c120
Yellow Bear <percussionpeng@gmail.com>
2024-04-02 15:48:33 -0700

    finish

--- app/src/main/java/com/example/viewpager2practice/TranslateFragment.kt
+++ app/src/main/java/com/example/viewpager2practice/TranslateFragment.kt
@@ -1,10 +1,14 @@
 package com.example.viewpager2practice
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.viewpager2practice.databinding.FragmentScaleBinding
+import com.example.viewpager2practice.databinding.FragmentTranslateBinding
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +21,21 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TranslateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentTranslateBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate, container, false)
+        _binding = FragmentTranslateBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TranslateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TranslateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageViewTranslate.setOnClickListener {
+            ObjectAnimator.ofFloat(it, "translationX", it.translationX + Random.nextInt(-100, 100)).start()
+            ObjectAnimator.ofFloat(it, "translationY", it.translationY + Random.nextInt(-100, 100)).start()
+        }
     }
 }
